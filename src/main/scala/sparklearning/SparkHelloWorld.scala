f679e4a9b6ad8b76b1860e29383f64584e5f4a2d
Denis Gontcharov <denis@gontcharov.eu>
2025-06-23 20:44:00 +0200

    Extend basic example file

--- src/main/scala/sparklearning/SparkHelloWorld.scala
+++ src/main/scala/sparklearning/SparkHelloWorld.scala
@@ -1,6 +1,7 @@
 package sparklearning
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions
 
 object SparkHelloWorld {
   def main(args: Array[String]): Unit = {
@@ -13,6 +14,7 @@ object SparkHelloWorld {
     val path = "src/main/resources/data/country_full.csv"
     val df = spark.read.option("header", "true").csv(path)
 
-    df.show()
+    val count_df = df.groupBy("region").agg(functions.count("region"))
+    count_df.show()
   }
 }
