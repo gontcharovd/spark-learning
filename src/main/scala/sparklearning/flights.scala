dbbab1273fa34f175fa948b19cb891d73b77cd59
Denis Gontcharov <denis@gontcharov.eu>
2025-07-07 17:57:49 +0200

    Write some Spark SQL

--- src/main/scala/sparklearning/flights.scala
+++ src/main/scala/sparklearning/flights.scala
@@ -14,7 +14,7 @@ object Flights {
 
   val schema = StructType(
     Array(
-      StructField("date", IntegerType),
+      StructField("date", StringType),
       StructField("delay", IntegerType),
       StructField("distance", IntegerType),
       StructField("origin", StringType),
@@ -22,7 +22,7 @@ object Flights {
     )
   )
 
-  val schema_str = """`date` INT, `delay` INT, `distance` INT,
+  val schema_str = """`date` STRING, `delay` INT, `distance` INT,
     `origin` STRING, `destination` STRING"""
 
   val flights_df = spark.read
@@ -34,7 +34,32 @@ object Flights {
 
   def main(args: Array[String]): Unit = {
     val head_df = spark.sql("SELECT * FROM flights_tbl LIMIT 5")
-    head_df.show()
+
+    val long_flights_df = spark.sql("""
+      SELECT date, distance, origin, destination
+      FROM flights_tbl
+      WHERE distance > 1000
+      ORDER BY distance DESC
+      LIMIT 10
+      """)
+
+    // long_flights_df.show()
+    
+    spark.sql("""
+      CREATE TABLE us_delay_flights_tbl(
+        date STRING,
+        delay INT,
+        distance INT,
+        origin STRING,
+        destination STRING
+      )
+      USING csv
+      OPTIONS (PATH 'data/flights/departuredelays.csv')
+    """)
+
+    val us_delay_flights_tbl = spark.sql("SELECT * FROM us_delay_flights_tbl LIMIT 5")
+
+    us_delay_flights_tbl.show()
 
     // clean shutdown
     spark.stop()
